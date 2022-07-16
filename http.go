097c89d444ef5dd800ae139abec9dd145373829a
Pourya <nlp.developer.c@gmail.com>
2022-07-16 17:11:20 +0300

    add Get/Post method
    Co-authored-by: Pouriya nlp.developer.c@gmail.com

--- http.go
+++ http.go
@@ -99,4 +99,11 @@ func main() {
 	if err := http.ListenAndServe(*addr, handler); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
+	
+	resp, err := http.Get("http://example.com/")
+
+	resp, err := http.Post("http://example.com/upload", "image/jpeg", &buf)
+	
+	resp, err := http.PostForm("http://example.com/form",
+		url.Values{"key": {"Value"}, "id": {"123"}})
 }
